15d46237788275fe8a835e48bd0aa573a1cbba66
Paulo Marques <pauloedums@gmail.com>
2021-03-26 13:06:24 -0300

    adding files and logic

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = (application) => {
     application.get('/', (req, res) => {
-        res.send('teste');
-    })
+        application.app.controllers.index.home(application, req, res);
+    });
 }
